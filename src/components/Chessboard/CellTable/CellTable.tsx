de853c928c6a4fbf7a87d1cded4f3ce8c395aeeb
b0006 <qsefthuken@gmail.com>
2022-08-29 22:32:28 +0700

    bugfix(board): square id

--- src/components/Chessboard/CellTable/CellTable.tsx
+++ src/components/Chessboard/CellTable/CellTable.tsx
@@ -5,16 +5,21 @@ import { DIGIT_LIST, SYMBOL_LIST } from '../constants';
 export const CellTable: FC = () => {
   return (
     <div className={styles.board}>
-      {SYMBOL_LIST.map((sym, symIndex) => (
-        <div key={sym} className={styles.row}>
-          {DIGIT_LIST.map((digit, digitIndex) => (
-            <div
-              key={`${sym}_${digit}`}
-              className={`${styles.cell} ${
-                (symIndex + digitIndex) % 2 === 0 ? styles.cell__light : styles.cell__dark
-              }`}
-            />
-          ))}
+      {DIGIT_LIST.map((digit, digitIndex) => (
+        <div key={digit} className={styles.row}>
+          {SYMBOL_LIST.map((sym, symIndex) => {
+            const squareId = `${sym}${digit}`;
+
+            return (
+              <div
+                key={squareId}
+                data-square={squareId}
+                className={`${styles.cell} ${
+                  (symIndex + digitIndex) % 2 === 0 ? styles.cell__light : styles.cell__dark
+                }`}
+              />
+            );
+          })}
         </div>
       ))}
     </div>
