9bade6c8fcb0cc93224840d83329c68e59f86ec8
b0006 <qsefthuken@gmail.com>
2023-05-07 23:55:49 +0700

    feature: init header buttons

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,11 +1,21 @@
 import { FC } from 'react';
+import { Button } from '../../common';
 import { Container } from '../Container';
 import styles from './Header.module.scss';
+import { RightContent } from './RightContent';
 
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <Container className={styles.inner}>ULTRA Chess</Container>
+      <Container className={styles.inner}>
+        <div className={styles.left}>
+          <Button href='/' icon='home' theme='flat' />
+        </div>
+        <div className={styles.center}>ULTRA chess</div>
+        <div className={styles.right}>
+          <RightContent />
+        </div>
+      </Container>
     </header>
   );
 };
---
+++ src/components/Layout/Header/RightContent/RightContent.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { Button } from '../../../common';
+import styles from './RightContent.module.scss';
+
+// TODO
+const IS_AUTH = false;
+
+const RightContent: FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.desktop}>
+        {IS_AUTH && (
+          <>
+            <Button href='/profile' text='Профиль' icon='profile' iconSide='right' />
+            <Button className={styles.button} text='Выход' icon='logout' iconSide='right' />
+          </>
+        )}
+        {!IS_AUTH && (
+          <>
+            <Button
+              className={styles.button}
+              href='/sign-in'
+              text='Sign in'
+              icon='signIn'
+              iconSide='right'
+              theme='secondary'
+            />
+            <Button
+              className={styles.button}
+              href='/sign-up'
+              text='Sign up'
+              icon='userPlus'
+              iconSide='right'
+              theme='primary'
+            />
+          </>
+        )}
+      </div>
+      <div className={styles.mobile}>
+        <Button icon='bars' theme='flat' />
+      </div>
+    </div>
+  );
+};
+
+export { RightContent };
